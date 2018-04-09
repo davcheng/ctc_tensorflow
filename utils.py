37b1484d6b60a87485332831211c7e8a1bab8e26
awsmdavid <awsmdavid@gmail.com>
2018-04-09 17:19:19 -0400

    updated utils.py to load words without .txt file

--- utils.py
+++ utils.py
@@ -18,7 +18,7 @@ except ImportError:
     raise ImportError
 
 # loops through folder of wavs and .txt targets
-def load_training_batch_data(input_path):
+def load_batch_data(input_path):
     import os
     import re
     batch_train_inputs = []
@@ -30,6 +30,9 @@ def load_training_batch_data(input_path):
             if not re.match('SA', file):
                 ext = os.path.splitext(file)[-1].lower()
                 filename = os.path.splitext(file)[-2].lower()
+                folder_name = os.path.split(subdir)[-1]
+                # folder_name = os.path.splitext(file)[-3].lower()
+                # For each wav file in folder
                 if ext in input_extensions:
                     train_fs, train_audio = wav.read(os.path.join(subdir, file))
                     train_inputs = mfcc(train_audio, samplerate=train_fs)
@@ -38,9 +41,19 @@ def load_training_batch_data(input_path):
                     # train_inputs = np.asarray(train_inputs[np.newaxis, :]) # giving this new axis makes appended shape go from (16,) to (144,13)
                     train_inputs = (train_inputs - np.mean(train_inputs))/np.std(train_inputs)
                     # train_seq_len = [train_inputs.shape[1]] # great for one example, but seq_len needs to be normalized for batch
+
                     # get the array index representation of the sentence label
+                    # Note: this requres all wav files to have either a .txt with a line for the transcription
+                    # OR requires the folder name of the sound file to be the label
                     try:
-                        train_targets = preprocess_file(os.path.join(subdir, filename+'.txt'))
+                        if os.path.exists(os.path.join(subdir, filename+'.txt')):
+                            train_targets = preprocess_file(os.path.join(subdir, filename+'.txt'))
+                        else:
+                            # NOTE: This requires all
+                            print('label .txt not in same file - parsing the folder directory for target')
+                            print(folder_name)
+                            train_targets = preprocess_text(folder_name)
+                            print(train_targets)
                     except:
                         print('issue with preprocessing ' + filename) # most likely because it cannot convert a phoneme
                         pass
@@ -54,12 +67,7 @@ def load_training_batch_data(input_path):
                         except:
                             print("ERROR WITH FILE " + file)
 
-    # batch_train_targets = np.vstack(a for a in batch_train_targets)
-    # batch_train_inputs = np.hstack(a for a in batch_train_inputs)
-    # batch_train_inputs = np.asarray([ for t in timesteps])
     # print('shape') # (413, num_examples) if using batch_train_inputs.append
-    # print(np.asarray(batch_train_targets).shape)
-    # print('yo')
     # print(np.asarray(batch_train_inputs).shape) # need this to be (num_examples,), eg. (16,)
     return np.asarray(batch_train_inputs), np.asarray(batch_train_targets)
 
@@ -205,10 +213,62 @@ def preprocess_file(target_filename):
                     new_targets.append(phoneme_dict[word.lower()])
                 except:
                     print(word.lower() + ' not in phoneme_dict')
+                    # pass
             else:
                 new_targets.append(" ")
         targets = [phone for new_targets in new_targets for phone in new_targets]
         # print(targets)
+
+    # Adding blank label (<space> instead of " "), removing commas
+    # ['SH' 'IY' '<space>' 'HH' 'AE' 'D' '<space>' 'Y' 'AO' 'R' '<space>' 'D'
+    #  'AA' 'R' 'K' '<space>' 'S' 'UW' 'T' '<space>' 'IH' 'N' '<space>' 'G' 'R'
+    #  'IY' 'S' 'IY' '<space>' 'W' 'AA' 'SH' '<space>' 'W' 'AO' 'T' 'ER'
+    #  '<space>' 'AO' 'L' '<space>' 'Y' 'IH' 'R']
+    targets = np.hstack([SPACE_TOKEN if x == ' ' else x for x in targets])
+
+    # Transform phoneme into index
+    # [30  1  0 39  4 35  0 19  9 17  0 35  8 17 38  0 28  6 36  0  2 21  0 37
+    # 17  1 28  1  0 18  8 30  0 18  9 36 15  0  9 16  0 19  2 17]
+    targets = np.asarray([SPACE_INDEX if x == SPACE_TOKEN else index_phone[x] for x in targets])
+    return targets
+
+# same as preprocess file, but takes a string input instead of a file with the label targets
+def preprocess_text(text):
+    SPACE_TOKEN = '<space>'
+    SPACE_INDEX = 0
+    phone_index = {1: 'IY', 2: 'IH', 3: 'EH', 4: 'AE', 5: 'AH', 6: 'UW', 7: 'UH', 8: 'AA', 9: 'AO', 10: 'EY', 11: 'AY', 12: 'OY', 13: 'AW', 14: 'OW', 15: 'ER', 16: 'L', 17: 'R', 18: 'W', 19: 'Y', 20: 'M', 21: 'N', 22: 'NG', 23: 'V', 24: 'F', 25: 'DH', 26: 'TH', 27: 'Z', 28: 'S', 29: 'ZH', 30: 'SH', 31: 'JH', 32: 'CH', 33: 'B', 34: 'P', 35: 'D', 36: 'T', 37: 'G', 38: 'K', 39: 'HH',' ': ' '}
+    index_phone = {'AA': 8, 'W': 18, 'DH': 25, 'Y': 19, 'HH': 39, 'B': 33, 'JH': 31, 'ZH': 29, 'D': 35, 'NG': 22, 'TH': 26, 'IY': 1, 'CH': 32, 'AE': 4, 'EH': 3, 'G': 37, 'F': 24, 'AH': 5, 'K': 38, 'M': 20, 'L': 16, 'AO': 9, 'N': 21, 'IH': 2, 'S': 28, 'R': 17, 'EY': 10, 'T': 36, 'AW': 13, 'V': 23, 'AY': 11, 'Z': 27, 'ER': 15, 'P': 34, 'UW': 6, 'SH': 30, 'UH': 7, 'OY': 12, 'OW': 14}
+
+    line = text
+
+    original = ' '.join(line.strip().lower().split(' ')).replace('.', '')
+    # original = ' '.join(line.strip().lower().split(' ')[2:]).replace('?', '')
+    # original = ' '.join(line.strip().lower().split(' ')[2:]).replace('!', '')
+    # regex to strip punctuation except apostrophe
+    original = re.sub(r"[^\w\s']",'',original)
+
+    # this adds a space so that when split, a space is preserved
+    # she  had  your  dark  suit  in  greasy  wash  water  all  year
+    targets = original.replace(' ', '  ')
+
+    # split into array of words
+    # ['she', '', 'had', '', 'your', '', 'dark', '', 'suit', '', 'in', '', 'greasy', '', 'wash', '', 'water', '', 'all', '', 'year']
+    targets = targets.split(' ')
+
+    # next convert array of words and spaces into phones and spaces
+    # ['SH', 'IY', ' ', 'HH', 'AE', 'D', ' ', 'Y', 'AO', 'R', ' ', 'D', 'AA', 'R', 'K', ' ', 'S', 'UW', 'T', ' ', 'IH', 'N', ' ', 'G', 'R', 'IY', 'S', 'IY', ' ', 'W', 'AA', 'SH', ' ', 'W', 'AO', 'T', 'ER', ' ', 'AO', 'L', ' ', 'Y', 'IH', 'R']
+    new_targets = []
+    for word in targets:
+        if len(word)>0:
+            try:
+                new_targets.append(phoneme_dict[word.lower()])
+            except:
+                print(word.lower() + ' not in phoneme_dict')
+                # pass
+        else:
+            new_targets.append(" ")
+    targets = [phone for new_targets in new_targets for phone in new_targets]
+    # print(targets)
 
     # Adding blank label (<space> instead of " "), removing commas
     # ['SH' 'IY' '<space>' 'HH' 'AE' 'D' '<space>' 'Y' 'AO' 'R' '<space>' 'D'
